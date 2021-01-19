aa7260d0116457e225da189bffa239228fe5df60
Steveycat <45834971+Steveycat@users.noreply.github.com>
2021-01-19 18:36:46 +0000

    Added deck creation and shuffle functions

--- BlackJack.cpp
+++ BlackJack.cpp
@@ -1,34 +1,41 @@
 #include <iostream>
-
+// Builds a card object
 struct card {
     char suit;
+    char rank[3];
     int value;
 };
-
-void new_deck(card* cards);
+// Builds a player's hand object
+struct player_hand {
+    int size;
+    int value;
+};
+// Declears functions
+void new_deck(card(&cards)[52]);
 void shuffle(card cards[], int n);
-
+// Main code execution
 int main()
 {
+    srand(time(0));
+
     // Array from 0 to 51 
     card cards[52];
+    card playerHand[5];
 
-    new_deck(*cards);
+    new_deck(cards);
     shuffle(cards, 52);
 
     // Printing all shuffled elements of cards 
     for (int i = 0; i < 52; i++)
-        std::cout << &cards[i] << " ";
+        std::cout << cards[i].rank << cards[i].suit << cards[i].value  << " ";
     std::cout << std::endl;
 
     return 0;
 }
-
+// Shuffle deck function
 void shuffle(card cards[], int n)
 {
     // Initialize seed randomly 
-    srand(time(0));
-
     for (int i = 0; i < n; i++)
     {
         // Random for remaining positions. 
@@ -37,22 +44,95 @@ void shuffle(card cards[], int n)
         std::swap(cards[i], cards[r]);
     }
 }
-
-void new_deck(card *cards) {
-    for (int i = 0; i < 13; i++)
+// Build deck function
+void new_deck(card(&cards)[52]) {
+    int i = 0;
+    int n = 0;
+    // Assigns suit and value
+    for (; i < 13; i++)
     {
         cards[i].suit = 'S';
+        cards[i].value = i % 13 + 1;
+        if (cards[i].value > 10) {
+            cards[i].value = 10;
+        }
     }
-    for (int i = 0; i < 13; i++)
+    for (; i < 26; i++)
     {
         cards[i].suit = 'H';
+        cards[i].value = i % 13 + 1;
+        if (cards[i].value > 10) {
+            cards[i].value = 10;
+        }
     }
-    for (int i = 0; i < 13; i++)
+    for (; i < 39; i++)
     {
         cards[i].suit = 'C';
+        cards[i].value = i % 13 + 1;
+        if (cards[i].value > 10) {
+            cards[i].value = 10;
+        }
     }
-    for (int i = 0; i < 13; i++)
+    for (; i < 52; i++)
     {
         cards[i].suit = 'D';
+        cards[i].value = i % 13 + 1;
+        if (cards[i].value > 10) {
+            cards[i].value = 10;
+        }
     }
+    // Assigns rank
+    for (int x = 0; x < 4; x++) {
+        i = 0;
+        for (i = 0; i < 13; i++)
+        {
+            switch (i) {
+            case 0:
+                strcpy_s(cards[n].rank, 3, "A");
+                break;
+            case 1:
+                strcpy_s(cards[n].rank, 3, "2");
+                break;
+            case 2:
+                strcpy_s(cards[n].rank, 3, "3");
+                break;
+            case 3:
+                strcpy_s(cards[n].rank, 3, "4");
+                break;
+            case 4:
+                strcpy_s(cards[n].rank, 3, "5");
+                break;
+            case 5:
+                strcpy_s(cards[n].rank, 3, "6");
+                break;
+            case 6:
+                strcpy_s(cards[n].rank, 3, "7");
+                break;
+            case 7:
+                strcpy_s(cards[n].rank, 3, "8");
+                break;
+            case 8:
+                strcpy_s(cards[n].rank, 3, "9");
+                break;
+            case 9:
+                strcpy_s(cards[n].rank, 3, "10");
+                break;
+            case 10:
+                strcpy_s(cards[n].rank, 3, "J");
+                break;
+            case 11:
+                strcpy_s(cards[n].rank, 3, "Q");
+                break;
+            case 12:
+                strcpy_s(cards[n].rank, 3, "K");
+                break;
+            }
+            n += 1;
+        }
+    }
+}
+
+void draw_card()
+{
+
 }
