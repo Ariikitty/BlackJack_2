3d68801e291db0ca82ed2e65b3bb9c228d60df4e
Steveycat <45834971+Steveycat@users.noreply.github.com>
2021-01-20 14:49:24 +0000

    Created header file, added menu, created draw screen function, added betting

--- BlackJack.cpp
+++ BlackJack.cpp
@@ -1,27 +1,7 @@
 #include <iostream>
-// Number of cards in play
-int cardsPlayed = 0;
-// Builds a card object
-struct card {
-    char suit;
-    char rank[3];
-    int value;
-};
-// Builds a player's hand object
-struct player_hand {
-    int size;
-    int value;
-};
-// Builds a dealer's hand object
-struct dealer_hand {
-    int size;
-    int value;
-};
-// Declears functions
-void new_deck(card(&cards)[52]);
-void shuffle(card cards[], int n);
-void draw_card(card playerCards[], card cards[], player_hand p_hand);
-void dealer_draw(card dealerCards[], card cards[], dealer_hand d_hand);
+#include <iomanip>
+#include <Windows.h>
+#include "BlackJack.h"
 // Main code execution
 int main()
 {
@@ -34,20 +14,64 @@ int main()
     // Initialises the hand structs
     player_hand p_hand{ 0, 0 };
     dealer_hand d_hand{ 0, 0 };
+    //Start's game
+    int choice = 0;
+    float balence = 500;
+    do {
+        system("cls");
+        std::cout << "Blackjack 21" << std::endl;
+        std::cout << "------------" << std::endl;
+        std::cout << "Your balence is " << balence << std::endl;
+        if (balence < 1)
+        {
+            std::cout << "You are out of money. Thanks for playing!" << std::endl;
+            Sleep(2000);
+            return 0;
+        }
+        std::cout << std::endl;
+        std::cout << "1: Quit game" << std::endl;
+        std::cout << "2: Play game" << std::endl;
+        std::cout << "3: How to play" << std::endl;
+        std::cout << std::endl;
+        std::cout << "What would you like to do (Please enter a number): ";
+        std::cin >> choice;
 
-    new_deck(cards);
-    shuffle(cards, 52);
-    draw_card(playerCards, cards, p_hand);
-    dealer_draw(dealerCards, cards, d_hand);
-
-    // Printing all shuffled elements of cards 
-    for (int i = 0; i < 52; i++)
-        std::cout << cards[i].rank << cards[i].suit << cards[i].value  << " ";
-    std::cout << std::endl;
-    std::cout << std::endl;
-    std::cout << playerCards[0].rank << playerCards[0].suit << std::endl;
-    std::cout << p_hand.value << std::endl;
-
+        switch (choice)
+        {
+        case 1:
+            std::cout << "Goodbye" << std::endl;
+            return 0;
+        case 2:
+            // Game code here please
+            float bet;
+            float pot;
+            system("cls");
+            std::cout << "How much would you like to bet? ";
+            std::cin >> bet;
+            balence -= bet;
+            pot = bet * 2;
+            new_deck(cards);
+            shuffle(cards, 52);
+            for (int i = 0; i < 2; i++) {
+                draw_card(playerCards, cards, p_hand);
+                dealer_draw(dealerCards, cards, d_hand);
+            }
+            draw_screen(dealerCards, d_hand, p_hand, playerCards, pot, balence);
+            Sleep(10000);
+            break;
+        case 3:
+            // How to play here
+            break;
+        default:
+            // There is a bug here if the user enters a letter, this will just loop. I'm not sure what causes it or how to fix it :c
+            system("cls");
+            std::cout << "You have entered an invalid option.\nPlease enter a number between 1 and 3!" << std::endl;
+            std::cout << std::endl;
+            Sleep(2000);
+            std::cin.clear();
+            choice = 0;
+        }
+    } while (choice != 1);
     return 0;
 }
 // Shuffle deck function
@@ -150,7 +174,7 @@ void new_deck(card(&cards)[52]) {
     }
 }
 // Function for player draw
-void draw_card(card playerCards[], card cards[], player_hand p_hand)
+void draw_card(card playerCards[], card cards[], player_hand &p_hand)
 {
     playerCards[p_hand.size] = cards[cardsPlayed];
     p_hand.value += playerCards[p_hand.size].value;
@@ -158,10 +182,31 @@ void draw_card(card playerCards[], card cards[], player_hand p_hand)
     cardsPlayed += 1;
 }
 // Function for dealer draw
-void dealer_draw(card dealerCards[], card cards[], dealer_hand d_hand)
+void dealer_draw(card dealerCards[], card cards[], dealer_hand &d_hand)
 {
     dealerCards[d_hand.size] = cards[cardsPlayed];
     d_hand.value += cards[cardsPlayed].value;
     d_hand.size += 1;
     cardsPlayed += 1;
+}
+// Function to draw the screen
+void draw_screen(card dealerCards[], dealer_hand d_hand, player_hand p_hand, card playerCards[], float pot, float balence)
+{
+    system("cls");
+    std::cout << std::setw(0) << "Dealer";
+    for (int i = 0; i < 5; i++) 
+    {
+        std::cout << std::setw(2) << dealerCards[i].rank << dealerCards[i].suit;
+    }
+    std::cout << std::setw(7) << "Total: ??";
+    std::cout << std::setw(7) << "Pot: " << pot << std::endl;
+    std::cout << std::endl;
+    std::cout << std::endl;
+    std::cout << std::setw(6) << "You";
+    for (int i = 0; i < 5; i++)
+    {
+        std::cout << std::setw(2) << playerCards[i].rank << playerCards[i].suit;
+    }
+    std::cout << std::setw(7) << "Total: " << p_hand.value;
+    std::cout << std::setw(11) << "Balence: " << balence << std::endl;
 }
---
+++ BlackJack.h
@@ -0,0 +1,25 @@
+#pragma once
+// Number of cards in play
+int cardsPlayed = 0;
+// Builds a card object
+struct card {
+    char suit;
+    char rank[3];
+    int value;
+};
+// Builds a player's hand object
+struct player_hand {
+    int size;
+    int value;
+};
+// Builds a dealer's hand object
+struct dealer_hand {
+    int size;
+    int value;
+};
+// Declears functions
+void new_deck(card(&cards)[52]);
+void shuffle(card cards[], int n);
+void draw_card(card playerCards[], card cards[], player_hand& p_hand);
+void dealer_draw(card dealerCards[], card cards[], dealer_hand& d_hand);
+void draw_screen(card dealerCards[], dealer_hand d_hand, player_hand p_hand, card playerCards[], float pot, float balence);
