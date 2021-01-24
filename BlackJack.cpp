8af123ec3105d766084dc643dabcc410f6eb6a67
Steveycat <45834971+Steveycat@users.noreply.github.com>
2021-01-24 16:07:01 +0000

    Added comments

--- BlackJack.cpp
+++ BlackJack.cpp
@@ -14,10 +14,12 @@ int main()
 	int choice = 0;
 	float balence = 500;
 	do {
+		// Writes the title of the program
 		system("cls");
 		std::cout << "Blackjack 21" << std::endl;
 		std::cout << "------------" << std::endl;
 		std::cout << "Your balance is " << balence << std::endl;
+		// This will check if the user has any money left. If they don't then the program will exit
 		if (balence < 1)
 		{
 			std::cout << "You are out of money. Thanks for playing!" << std::endl;
@@ -31,6 +33,7 @@ int main()
 		player_hand p_hand{ 0, 0 };
 		dealer_hand d_hand{ 0, 0 };
 		std::cout << std::endl;
+		// The main menu
 		std::cout << "1: Quit game" << std::endl;
 		std::cout << "2: Play game" << std::endl;
 		std::cout << "3: How to play" << std::endl;
@@ -38,16 +41,17 @@ int main()
 		std::cout << std::endl;
 		std::cout << "What would you like to do (Please enter a number): ";
 		std::cin >> choice;
-
+		// Switch case for the main menu choices
 		switch (choice)
 		{
 		case 1:
 			std::cout << "Goodbye" << std::endl;
 			return 0;
 		case 2:
-			// Game code here please
-			gamePlaying = 1;
-			playerTurn = 1;
+			// Game code here
+			gamePlaying = 1; // Set's the game playing as true
+			playerTurn = 1; // Set's the player's turn to true
+			// Initalises the betting system
 			float bet;
 			float pot;
 			system("cls");
@@ -56,15 +60,21 @@ int main()
 			balence -= bet;
 			pot = bet * 2;
 			playerWinState = 4;
+			// Creates a new deck of cards
 			new_deck(cards);
+			// Shuffles the cards
 			shuffle(cards, 52);
+			// The first draw of two cards for both the dealer and the player
 			for (int i = 0; i < 2; i++) {
 				draw_card(playerCards, cards, p_hand);
 				dealer_draw(dealerCards, cards, d_hand);
 			}
+			// Checks to see if anyone has an ace and if they do then it will convert it to an 11 unless it will cause the user to go over
 			ace_rule(playerCards, p_hand, dealerCards, d_hand);
 			do {
+				// Asks the player if they want to hit or stick
 				int choice = 0;
+				// draw_screen will draw the screen with the set UI layout
 				draw_screen(dealerCards, d_hand, p_hand, playerCards, pot, balence, cards);
 				std::cout << std::endl;
 				std::cout << "You are under by " << 21 - p_hand.value << std::endl;
@@ -76,44 +86,78 @@ int main()
 				switch (choice)
 				{
 				case 1:
+					// When the player wants to hit, they will draw a new card, check for aces and then checks to see if the player has won
 					draw_card(playerCards, cards, p_hand);
 					ace_rule(playerCards, p_hand, dealerCards, d_hand);
 					win_check(p_hand, d_hand);
 					break;
 				case 2:
+					// Ends the player's turn and starts the dealer's turn
 					playerTurn = 0;
 				}
+			// Will keep going until the player ends their turn
 			} while (playerTurn == 1);
+			// His to check if the player has won or gone bust before playing
 			if (playerWinState == 4) {
+				// If the dealer is more then 4 away from 21 then they will keep drawing
 				while (21 - d_hand.value > 4) {
+					// Same as the player's just with the dealer's hand instead
 					dealer_draw(dealerCards, cards, d_hand);
 					ace_rule(playerCards, p_hand, dealerCards, d_hand);
 					win_check(p_hand, d_hand);
 				};
 			}
+			// Draws the screen with the UI of the game
 			draw_screen(dealerCards, d_hand, p_hand, playerCards, pot, balence, cards);
+			// Gameend_check is similar to win_check but is only called after the game to see who wins on value
 			gameend_check(p_hand, d_hand);
+			// Switch statement to decide on how the player finished the game
 			switch (playerWinState)
 			{
 			case 0:
+				// Called when the dealer scores higher then the player
 				std::cout << "You lost, better luck next time!";
 				break;
 			case 1:
+				// Called when the player goes bust
 				std::cout << "You went bust, be more careful next time!";
 				break;
 			case 2:
+				// Called when the player gets exactly 21, has 5 cards in their hand without going bust or scores higher then the dealer
 				std::cout << "You won!! You got " << pot;
 				balence += pot;
 				break;
 			case 3:
+				// Called when the player and the dealer score the exact same value
 				std::cout << "You both got the same value, you neither won or lost";
 				balence += bet;
 				break;
 			}
+			// Pauses on the game end screen for 5 seconds before continuing back to the menu
 			Sleep(5000);
 			break;
 		case 3:
 			// How to play here
+			system("cls");
+			std::cout << "Welcome to balckjack!!" << std::endl;
+			std::cout << std::endl;
+			Sleep(2000);
+			std::cout << "How to play" << std::endl;
+			std::cout << "-----------" << std::endl;
+			std::cout << "This game rquires you to get a hand of 21." << std::endl;
+			std::cout << "Going over 21 will cause you to bust and you will lose the round" << std::endl;
+			std::cout << "If you get exactly 21 or have 5 cards in your hand without going bust then you automatically win the round" << std::endl;
+			std::cout << std::endl;
+			Sleep(5000);
+			std::cout << "To help you get to 21, you can choose to hit or stick" << std::endl;
+			std::cout << "Hitting will cause you to draw another card from the deck" << std::endl;
+			std::cout << "Sticking will keep your current hand and end your turn so only do this if you think you are going to go over 21" << std::endl;
+			std::cout << std::endl;
+			Sleep(5000);
+			std::cout << "Every card is worth their face value" << std::endl;
+			std::cout << "Aces are worth either 1 or 11 depending on if you will go bust or not" << std::endl;
+			std::cout << "Jacks, Queens and Kings are all worth 10 points" << std::endl;
+			Sleep(10000);
 			break;
 		case 4:
 			system("cls");
@@ -312,15 +356,15 @@ void win_check(player_hand p_hand, dealer_hand d_hand)
 		gamePlaying = 0;
 		playerWinState = 2;
 	}
-	else if (p_hand.size == 5) {
-		playerTurn = 0;
-		gamePlaying = 0;
-		playerWinState = 2;
-	}
 	else if (p_hand.value > 21) {
 		playerTurn = 0;
 		gamePlaying = 0;
 		playerWinState = 1;
+	}
+	else if (p_hand.size == 5) {
+		playerTurn = 0;
+		gamePlaying = 0;
+		playerWinState = 2;
 	}
 	else if (d_hand.value > 21) {
 		gamePlaying = 0;
